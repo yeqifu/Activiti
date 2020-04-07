5a120b3643d715d990575e30f6b5180a6094ab50
yeqifu <1784525940@qq.com>
2020-04-07 23:50:52 +0800

    activiti

---
+++ src/test/java/com/yeqifu/test/ActivitiTest.java
@@ -0,0 +1,15 @@
+package com.yeqifu.test;
+
+/**
+ * @Author: 落亦-
+ * @Date: 2020/4/7 23:23
+ */
+public class ActivitiTest {
+
+    @Test
+    public void testGenerationTable(){
+        //1.创建ProcessEngineConfiguration对象
+
+    }
+
+}
